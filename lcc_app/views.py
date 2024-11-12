78eebd423dd22d788d0a16726a99fdd3d1cf8054
Lucianooli <lucianooliveira19oliveira@gmail.com>
2024-11-12 13:04:42 -0300

    Introdução a programação pronta

--- lcc_app/views.py
+++ lcc_app/views.py
@@ -104,6 +104,9 @@ def Matematica_Discreta(request):
 def Introducao_programacao(request):
     pdf_1 = Pdf.objects.get(id=23)
     pdf_2 = Pdf.objects.get(id=24)
+    pdf_3 = Pdf.objects.get(id=25)
     desafio_1 = Desafio.objects.get(id=9)
     desafio_2 = Desafio.objects.get(id=10)
-    return render(request,'lcc_app/introducao_programacao.html',{"pdf_1":pdf_1,"desafio_1":desafio_1,"pdf_2":pdf_2,"desafio_2":desafio_2,})
+    desafio_3 = Desafio.objects.get(id=11)
+
+    return render(request,'lcc_app/introducao_programacao.html',{"pdf_1":pdf_1,"desafio_1":desafio_1,"pdf_2":pdf_2,"desafio_2":desafio_2,"pdf_3":pdf_3,"desafio_3":desafio_3})
